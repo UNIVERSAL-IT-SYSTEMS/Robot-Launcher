52675d7f6e541dd0545e9a4868aa5bb56b573d07
Alexander01998 <contact.wurstimperium@gmail.com>
2015-04-06 16:34:50 +0200

    Initial commit of source code

---
+++ Robot Launcher/src/tk/wurst_client/robot_launcher/MainFrame.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright © 2015 | Alexander01998 | All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package tk.wurst_client.robot_launcher;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame
+{
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				try
+				{
+					MainFrame frame = new MainFrame();
+					frame.setVisible(true);
+				}catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainFrame()
+	{
+		setBounds(100, 100, 450, 300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
